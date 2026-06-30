9922afa6aabfc29a6ead99a7d514271efd4fa8a4
Sona <sonashek2004@gmail.com>
2026-06-30 16:19:49 +0530

    operator

---
+++ src/operator.py
@@ -0,0 +1,29 @@
+#arithmetic
+
+a=1
+b=2
+print(a+b)
+
+a=10
+b=5
+print(a//b)#floor division
+
+a=10
+b=5
+print(a/b)#floor division
+
+##assignment 
+a=1
+print(a)
+
+#membership in not in
+
+a="sona"
+print('p' not in a )
+
+
+#bitwise operator
+
+a=2
+b=3
+print(a>>1)

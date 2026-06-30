85999af3f34b0cb0d7e2bb9ad7b44eb0ad8dcc5f
Sona <sonashek2004@gmail.com>
2026-06-30 14:56:47 +0530

    variable working

--- src/Variable.py
+++ src/Variable.py
@@ -1,1 +1,24 @@
+#Variable assigning
+a=10
+b=21
+print(a+b)
 
+
+##reassigning variable
+
+a=10
+a=11
+print (a)
+
+#assigning multiple value to multiple variable
+a,b,c=10,11,12
+print(a,b,c)
+
+#Assigning the Same Value
+x=y=z=100
+print(x,y,z)
+
+#Swapping Variables
+aa,bb=1,2
+aa,bb=bb,aa
+print(aa,bb)
